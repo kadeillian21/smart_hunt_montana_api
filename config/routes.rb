fa6c773d6e28f49e955456148b812c39f2145fb0
Kade Illian <kadeillianmt@gmail.com>
2022-12-02 13:33:42 -0600

    added resources for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :antelopes, except: [:create, :update, :destroy]
+  resources :black_bears, except: [:create, :update, :destroy]
+  resources :elks, except: [:create, :update, :destroy]
+  resources :mooses, except: [:create, :update, :destroy]
+  resources :mountain_goats, except: [:create, :update, :destroy]
+  resources :mountain_lions, except: [:create, :update, :destroy]
+  resources :mule_deers, except: [:create, :update, :destroy]
+  resources :sheeps, except: [:create, :update, :destroy]
+  resources :turkeys, except: [:create, :update, :destroy]
+  resources :upland_game, except: [:create, :update, :destroy]
+  resources :whitetail_deers, except: [:create, :update, :destroy]
+  resources :wolves, except: [:create, :update, :destroy]
 end
